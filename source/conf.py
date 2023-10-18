5197ee2204e1eeefc1cd240d7d8e1d254644f224
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-10-17 21:05:09 -0500

    Clean up configuration file

--- source/conf.py
+++ source/conf.py
@@ -6,34 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-def add_module_path():
-    from pathlib import Path
-    import sys
-
-    path = (Path.cwd() / ".." / ".." / "src").resolve()
-
-    sys.path.insert(0, str(path))
-
 def get_module_version():
-    from pathlib import Path
-    import sys
-
-    path = Path(sys.path[0]) / "jsnake" / "__init__.py"
-    source = path.read_text()
-
-    import ast
-    code = ast.parse(source, __file__)
-
-    for node in code.body:
-        match node:
-            case ast.Assign(targets=[ast.Name(id=name, ctx=ast.Store())],
-                            value=ast.Constant(value=value)):
-                if name == "__version__":
-                    return value
-
-    raise RuntimeError(f"failed to extract version from {path}")
-
-add_module_path()
+    import jsnake
+    return jsnake.__version__
 
 project = 'JSnake'
 copyright = '2023, JohnDevlopment'
